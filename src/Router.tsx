dd2b4f042dda3b2b6db7f979a69490509fb1378f
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-06-28 22:35:22 -0300

    added route details

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { Subscribe } from "./pages/Subscribe";
 import { Dashboard } from "./pages/Dashboard";
+import { Details } from "./pages/Details";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Subscribe />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/details" element={<Details />} />
     </Routes>
   );
 }
