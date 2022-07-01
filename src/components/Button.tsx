244a6a57b10d74c8a5c9e637cc7f8b24c07b1d9b
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-07-01 11:33:26 -0300

    migrated styles to stitches CSS in JS

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,76 @@
-import clsx from "clsx";
-import { ButtonHTMLAttributes } from "react";
+import { styled } from "../../stitches.config";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  className?: string;
-}
+export const Button = styled("button", {
+  height: 50,
+  padding: "0 20px",
+  borderRadius: 4,
+  fontSize: 16,
+  fontWeight: 700,
+  textTransform: "uppercase",
 
-export function Button({ className, children, ...props }: ButtonProps) {
-  return (
-    <button
-      {...props}
-      className={clsx(
-        `h-[50px] px-5 border border-black uppercase font-bold transition-colors rounded`,
-        className
-      )}
-    >
-      {children}
-    </button>
-  );
-}
+  variants: {
+    shape: {
+      primary: {
+        background: "Black",
+        color: "White",
+        border: "1px solid Black",
+        transition: ".2s all",
+
+        "&:hover": {
+          filter: "brightness(0.9)"
+        },
+
+        "&:focus": {
+          outline: "none"
+        }
+      },
+
+      secondary: {
+        background: "transparent",
+        color: "Black",
+        border: "1px solid Black",
+        transition: ".2s all",
+        "&:hover": {
+          background: "Black",
+          color: "White"
+        }
+      }
+    },
+
+    color: {
+      green: {
+        background: "#34CB79"
+      }
+    },
+
+    danger: {
+      true: {}
+    }
+  },
+
+  compoundVariants: [
+    {
+      shape: "secondary",
+      danger: true,
+      css: {
+        "&:hover": {
+          background: "#dc2626",
+          color: "White",
+          border: "1px solid #dc2626"
+        }
+      }
+    },
+
+    {
+      shape: "primary",
+      color: "green",
+      css: {
+        borderColor: "#34CB79"
+      }
+    }
+  ],
+
+  defaultVariants: {
+    shape: "primary"
+  }
+});
