3c5d38f8811a927f89fd5b8765e6b92e20104c1c
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-07-11 13:00:27 -0300

    added function authenticate

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -7,7 +7,8 @@ import {
 } from "react";
 
 import { useNavigate } from "react-router-dom";
-import { IUser } from "../types/user";
+import { toast } from "react-toastify";
+import { IUser } from "../types";
 import { getUserLocalStorage, setUserLocalStorage } from "../utils";
 
 interface UserContextProviderProps {
@@ -18,6 +19,7 @@ interface UserContextData {
   user: IUser | null;
   signUp: (user: IUser) => Promise<void>;
   signOut: () => void;
+  authenticate: (email: string, password: string) => Promise<void>;
 }
 
 const UserContext = createContext({} as UserContextData);
@@ -40,14 +42,30 @@ export function UserContextProvider({ children }: UserContextProviderProps) {
   }
 
   function signOut() {
-    // setUserLocalStorage(null);
     setUser(null);
 
     navigate("/");
   }
 
+  async function authenticate(email: string, password: string) {
+    const user = getUserLocalStorage() as IUser;
+
+    if (user) {
+      if (user.email === email && user.password === password) {
+        setUser({ email, password });
+        setUserLocalStorage({ email, password });
+
+        navigate("/dashboard");
+      } else {
+        toast.error("Login ou senha inválidos", { theme: "colored" });
+      }
+    } else {
+      toast.error("Usuário não encontrado", { theme: "colored" });
+    }
+  }
+
   return (
-    <UserContext.Provider value={{ user, signUp, signOut }}>
+    <UserContext.Provider value={{ user, signUp, signOut, authenticate }}>
       {children}
     </UserContext.Provider>
   );
