0c0a4e6999461f80d855872dde0605c4751f2e2b
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-07-22 11:54:29 -0300

    added actions UPDATE_MEMBER_PAYMENT_STATUS and CALCULATE_CONTRIBUTION_MEMBERS

--- src/reducers/barbecue/actions.ts
+++ src/reducers/barbecue/actions.ts
@@ -4,6 +4,8 @@ import { IBarbecue } from "../../types";
 export enum ActionsType {
   ADD_NEW_BARBECUE = "ADD_NEW_BARBECUE",
   ADD_NEW_MEMBER_T0_BARBECUE = "ADD_NEW_MEMBER_T0_BARBECUE",
+  UPDATE_MEMBER_PAYMENT_STATUS = "UPDATE_MEMBER_PAYMENT_STATUS",
+  CALCULATE_CONTRIBUTION_MEMBERS = "CALCULATE_CONTRIBUTION_MEMBERS",
 }
 
 export function addNewBarbecueAction(newBarbecue: IBarbecue) {
@@ -23,7 +25,29 @@ export function addNewMemberToBarbecueAction(
     type: ActionsType.ADD_NEW_MEMBER_T0_BARBECUE,
     payload: {
       newMember,
-      barbecueListId
+      barbecueListId,
+    },
+  };
+}
+
+export function updateMemberPaymentStatusAction(
+  memberId: string,
+  barbecueListId: string
+) {
+  return {
+    type: ActionsType.UPDATE_MEMBER_PAYMENT_STATUS,
+    payload: {
+      memberId,
+      barbecueListId,
+    },
+  };
+}
+
+export function calculateContributionMembersAction(barbecueListId: string) {
+  return {
+    type: ActionsType.CALCULATE_CONTRIBUTION_MEMBERS,
+    payload: {
+      barbecueListId,
     },
   };
 }
--- src/reducers/barbecue/reducer.ts
+++ src/reducers/barbecue/reducer.ts
@@ -15,13 +15,48 @@ export function barbecuesReducer(state: BarbecuesState, action: any) {
 
   if (action.type === ActionsType.ADD_NEW_MEMBER_T0_BARBECUE) {
     return produce(state, (draft) => {
-      const barbecue = draft.barbecues.find((barbecue) => {
+      const barbecueExists = draft.barbecues.find((barbecue) => {
         return barbecue.id === action.payload.barbecueListId ? barbecue : null;
       });
 
-      barbecue?.members.push(action.payload.newMember);
+      barbecueExists?.members.push(action.payload.newMember);
     });
   }
 
-  return state;
+  if (action.type === ActionsType.UPDATE_MEMBER_PAYMENT_STATUS) {
+    return produce(state, (draft) => {
+      const barbecueExists = draft.barbecues.find((barbecue) => {
+        return barbecue.id === action.payload.barbecueListId ? barbecue : null;
+      });
+
+      if (barbecueExists != undefined) {
+        const member: any = barbecueExists.members.find(
+          (member) => member.id === action.payload.memberId
+        );
+
+        member.paid = !member.paid;
+      }
+    });
+  }
+
+  if (action.type === ActionsType.CALCULATE_CONTRIBUTION_MEMBERS) {
+    return produce(state, (draft) => {
+      const barbecueExists = draft.barbecues.find(
+        (barbecue) => barbecue.id === action.payload.barbecueListId
+      );
+
+      if (barbecueExists != undefined) {
+        const membersContribution = barbecueExists.members.filter(member => member.paid === true)
+        .reduce((acc, member) => {
+          const drinkValue = member.hasDrinkIncluded ? 20 : 0
+          
+          return acc + member.contribution + drinkValue
+        }, 0)
+
+        barbecueExists.totalAmountCollected = membersContribution
+      }
+    });
+  }
+
+  return state; 
 }
