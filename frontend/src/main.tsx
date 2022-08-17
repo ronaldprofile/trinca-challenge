e8d4907ea8660f2e328a6b3375bb57b62bb20184
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-08-17 17:29:10 -0300

    configured axios and react query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/Auth/AuthProvider";
 import { BarbecueContextProvider } from "./context/Barbecue/BarbecueProvider";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./services/queryClient";
+import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { App } from "./App";
 
@@ -12,12 +15,16 @@ import "react-toastify/dist/ReactToastify.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthContextProvider>
-        <BarbecueContextProvider>
-          <App />
-        </BarbecueContextProvider>
-        <ToastContainer />
-      </AuthContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthContextProvider>
+          <BarbecueContextProvider>
+            <App />
+          </BarbecueContextProvider>
+          <ToastContainer />
+        </AuthContextProvider>
+
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
