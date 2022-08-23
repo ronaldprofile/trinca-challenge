52a02093aa9c94264e7a71941323fb2ad0c589a3
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-08-23 17:17:36 -0300

    added component Loading

---
+++ frontend/src/components/Loading/index.tsx
@@ -0,0 +1,12 @@
+import { LoadingContainer } from "./styles";
+
+export function Loading() {
+  return (
+    <LoadingContainer>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </LoadingContainer>
+  );
+}
---
+++ frontend/src/components/Loading/styles.ts
@@ -0,0 +1,44 @@
+import { keyframes, styled } from "../../../stitches.config";
+
+const animationLoading = keyframes({
+  to: {
+    transform: "scaleY(2)",
+  },
+});
+
+export const LoadingContainer = styled("div", {
+  width: "100vw",
+  height: "100vh",
+  position: "fixed",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  background: "rgba(0, 0, 0, 0.7)",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+
+  span: {
+    width: "1rem",
+    height: "3.125rem",
+    margin: "0 0.125rem",
+    background: "#FFD836",
+    display: "inline-block",
+
+    "&:nth-child(1)": {
+      animation: `${animationLoading} 0.5s -0.45s ease-in-out alternate infinite`,
+    },
+
+    "&:nth-child(2)": {
+      animation: `${animationLoading} 0.5s -0.3s ease-in-out alternate infinite`,
+    },
+
+    "&:nth-child(3)": {
+      animation: `${animationLoading} 0.5s -0.15s ease-in-out alternate infinite`,
+    },
+
+    "&:nth-child(4)": {
+      animation: `${animationLoading} 0.5s 0s ease-in-out alternate infinite`,
+    },
+  },
+});
