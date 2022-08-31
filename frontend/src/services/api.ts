e44ecb5e1003305b3fbac0335f7c436f211824f2
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-08-31 17:30:59 -0300

    fix: name variable

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = import.meta.env.API_URL || "http://localhost:4000";
+const url = import.meta.env.VITE_API_URL || "http://localhost:4000";
 
 export const api = axios.create({
   baseURL: url,
