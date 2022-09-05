ba4718c9f108f522af0dbd4ee4077ae1f3eda551
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-09-05 08:47:56 -0300

    added lib react hook form

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import { Subscribe } from "./pages/Subscribe";
+import { Login } from "./pages/Login";
 import { Dashboard } from "./pages/Dashboard";
 import { Details } from "./pages/Details";
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Subscribe />} />
+      <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/details/:id" element={<Details />} />
     </Routes>
